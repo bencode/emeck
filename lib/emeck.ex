223ef5e2669deb5ef05af2de7403a6e7fe63c6ce
bencode <bencode@163.com>
2017-05-13 14:03:39 +0800

    easy passthrough call

--- lib/emeck.ex
+++ lib/emeck.ex
@@ -24,11 +24,13 @@ defmodule Emeck do
         Enum.map(mods, &:meck.unload/1)
       end
     end
+    :ok
   end
 
 
   defmodule Helper do
-    defmacro spy({{:., _, [m, f]}, _, []}, proxy) do
+    # expect(Foo.bar, fn)
+    defmacro expect({{:., _, [m, f]}, _, []}, proxy) do
       quote do
         :meck.expect(unquote(m), unquote(f), unquote(proxy))
       end
@@ -46,7 +48,31 @@ defmodule Emeck do
         :meck.passthrough(unquote(list))
       end
     end
-
+    defmacro passthrough(a) do
+      quote do
+        passthrough([unquote(a)])
+      end
+    end
+    defmacro passthrough(a, b) do
+      quote do
+        passthrough([unquote(a), unquote(b)])
+      end
+    end
+    defmacro passthrough(a, b, c) do
+      quote do
+        passthrough([unquote(a), unquote(b), unquote(c)])
+      end
+    end
+    defmacro passthrough(a, b, c, d) do
+      quote do
+        passthrough([unquote(a), unquote(b), unquote(c), unquote(d)])
+      end
+    end
+    defmacro passthrough(a, b, c, d, e) do
+      quote do
+        passthrough([unquote(a), unquote(b), unquote(c), unquote(d), unquote(e)])
+      end
+    end
 
     defmacro called({{:., _, [m, f]}, _, []}) do
       quote do
@@ -71,6 +97,21 @@ defmodule Emeck do
       quote do
         :meck.num_calls(unquote(m), unquote(f), unquote(args))
       end
+    end
+
+    defmacro call_args do
+    end
+
+    defmacro call_return do
+    end
+
+    defmacro calls do
+    end
+
+    defmacro first_call do
+    end
+
+    defmacro last_call do
     end
   end
 end
--- test/emeck_test.exs
+++ test/emeck_test.exs
@@ -5,31 +5,68 @@ defmodule EmeckTest do
 
   test "simple mock" do
     with_meck String do
-      spy String.length, fn s ->
-        passthrough([s]) * 2
+      expect String.length, fn s ->
+        passthrough(s) * 2
       end
 
-      assert String.length("abc") == 6
+      assert String.length("foo") == 6
 
       assert called String.length
-      assert called String.length("abc")
-      refute called String.length("abd")
+      assert called String.length("foo")
+      refute called String.length("bar")
 
       assert call_count(String.length) == 1
-      assert call_count(String.length("abc")) == 1
+      assert call_count(String.length("foo")) == 1
 
-      String.length("abd")
-      assert called String.length("abd")
+      String.length("bar")
+      assert called String.length("bar")
       assert call_count(String.length) == 2
     end
 
-    assert String.length("abc") == 3
+    assert String.length("foo") == 3
   end
 
 
-  test "multiple mods" do
-    with_meck [String] do
-      assert 1 == 1
+  test "muti modules" do
+    with_meck [String, Path] do
+      expect String.length, &passthrough(&1)
+      expect Path.join, &passthrough(&1, &2)
+
+      assert String.length("foo") == 3
+      assert called String.length
+
+      assert Path.join("foo", "bar") == "foo/bar"
+    end
+
+    with_meck String do
+      expect String.length, &(passthrough(&1) + 1)
+      assert String.length("foo") == 4
+    end
+    assert String.length("foo") == 3
+  end
+
+
+  defmodule Foo do
+    def bar(a), do: a
+    def bar(a, b), do: [a, b]
+    def bar(a, b, c), do: [a, b, c]
+    def bar(a, b, c, d), do: [a, b, c, d]
+    def bar(a, b, c, d, e), do: [a, b, c, d, e]
+  end
+
+  test "passthrough with diffent arity" do
+    with_meck Foo do
+      expect Foo.bar, &passthrough(&1)
+      expect Foo.bar, &passthrough(&1, &2)
+      expect Foo.bar, &passthrough(&1, &2, &3)
+      expect Foo.bar, &passthrough(&1, &2, &3, &4)
+      expect Foo.bar, &passthrough(&1, &2, &3, &4, &5)
+
+      assert Foo.bar("a") == "a"
+      assert Foo.bar("a", "b") == ["a", "b"]
+      assert Foo.bar("a", "b", "c") == ["a", "b", "c"]
+      assert Foo.bar("a", "b", "c", "d") == ["a", "b", "c", "d"]
+      assert Foo.bar("a", "b", "c", "d", "e") == ["a", "b", "c", "d", "e"]
     end
   end
 end
