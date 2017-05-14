6a05dfb27dc5e86bb9069ffe9b9ad728022b039e
bencode <bencode@163.com>
2017-05-14 18:18:02 +0800

    upate mix

--- mix.exs
+++ mix.exs
@@ -9,34 +9,29 @@ defmodule Emeck.Mixfile do
       elixirc_paths: elixirc_paths(Mix.env),
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
+      description: description(),
       deps: deps(),
+      package: package(),
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
+      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
+      name: "emeck",
+      source_url: "https://github.com/bencode/emeck"
     ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
     [extra_applications: [:logger]]
   end
-
 
   defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
   defp elixirc_paths(_),     do: ["lib", "web"]
 
+  defp description do
+    """
+    A Mocking library for Elixir language. It's implemented based on meck.
+    """
+  end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:meck, "~> 0.8"},
@@ -44,5 +39,15 @@ defmodule Emeck.Mixfile do
       {:httpoison, "~> 0.11", only: :test},
       {:excoveralls, "~> 0.5", only: :test}
     ]
+  end
+
+  defp package do
+    %{
+      licenses: ["MIT"],
+      maintainers: ["bencode"],
+      links: %{
+        "GitHub" => "https://github.com/bencode/emeck"
+      }
+    }
   end
 end
