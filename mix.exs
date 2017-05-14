933352aec45ce49fe87776565a76ff622206022d
bencode <bencode@163.com>
2017-05-14 18:46:13 +0800

    bump version to 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Emeck.Mixfile do
   def project do
     [
       app: :emeck,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.3",
       elixirc_paths: elixirc_paths(Mix.env),
       build_embedded: Mix.env == :prod,
